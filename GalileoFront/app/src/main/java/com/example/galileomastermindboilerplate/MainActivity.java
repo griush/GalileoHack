774d364e293112d47a9d659b942d38693d9f1f31
Martí Climent <marticlilop@gmail.com>
2024-08-10 02:35:07 +0200

    Add materialyou theme

--- GalileoFront/app/src/main/java/com/example/galileomastermindboilerplate/MainActivity.java
+++ GalileoFront/app/src/main/java/com/example/galileomastermindboilerplate/MainActivity.java
@@ -46,8 +46,6 @@ public class  MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-
-
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -55,6 +53,7 @@ public class  MainActivity extends AppCompatActivity {
                 R.id.landingPage, R.id.generalStatsPage, R.id.satelliteListPage)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
 
