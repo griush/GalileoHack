87de7d8980bd616767f8b20230ca505a169af8b2
Martí Climent <marticlilop@gmail.com>
2024-08-12 00:06:44 +0200

    Drop SDK to 30, upgrade java from J1.8 -> J17

--- GalileoFront/app/build.gradle.kts
+++ GalileoFront/app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.galileomastermindboilerplate"
-        minSdk = 31
+        minSdk = 30
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -27,8 +27,8 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     buildFeatures {
         viewBinding = true
