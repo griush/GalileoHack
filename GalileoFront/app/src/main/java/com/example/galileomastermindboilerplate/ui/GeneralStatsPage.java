0fc88be64e3819b06d99b3d22bddf347716f8097
griush <gerardriush@gmail.com>
2024-08-26 18:14:15 +0200

    stats page refreshes automatically

--- GalileoFront/app/src/main/java/com/example/galileomastermindboilerplate/ui/GeneralStatsPage.java
+++ GalileoFront/app/src/main/java/com/example/galileomastermindboilerplate/ui/GeneralStatsPage.java
@@ -2,15 +2,12 @@ package com.example.galileomastermindboilerplate.ui;
 
 import android.app.Activity;
 import android.location.GnssCapabilities;
-import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,16 +16,32 @@ import android.widget.TextView;
 import com.example.galileomastermindboilerplate.R;
 
 import java.util.Locale;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class GeneralStatsPage extends Fragment {
 
     public static GnssCapabilities Capabilities = null;
     public static String GnssModelName = null;
     public static int GnssModelYear = 0; // 0 is Unknown
-    public static double Latitude = 0.0, Longitude = 0.0 , Altitude = 0.0, Accuracy = 0.0;
+    public static double Latitude = 0.0, Longitude = 0.0, Altitude = 0.0, Accuracy = 0.0;
     public static float Bearing = 0.0f, BearingAccuracy = 0.0f;
 
-    SwipeRefreshLayout PullToRefresh;
+    TextView supportsNavigationMessages = null;
+    TextView supportsMeasurements = null;
+    TextView hasADR = null;
+    TextView hasMsa = null;
+    TextView supportsSatellitePvt = null;
+    TextView hardwareName = null;
+    TextView hardwareYear = null;
+    TextView deviceLatitude = null;
+    TextView deviceLongitude = null;
+    TextView deviceAltitude = null;
+    TextView deviceAccuracy = null;
+    TextView deviceBearing = null;
+    TextView deviceBearingAccuracy = null;
+
+    private Timer mRefreshTimer;
 
     public static GeneralStatsPage newInstance() {
         GeneralStatsPage fragment = new GeneralStatsPage();
@@ -49,43 +62,52 @@ public class GeneralStatsPage extends Fragment {
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        PullToRefresh = view.findViewById(R.id.SwipeToRefreshLocation);
-        PullToRefresh.setOnRefreshListener(() -> {
-            DrawCapabilities(view);
-            PullToRefresh.setRefreshing(false);
-        });
+        super.onViewCreated(view, savedInstanceState);
 
-        DrawCapabilities(view);
+        // Setup UI
+        supportsNavigationMessages = view.findViewById(R.id.supportsNavigationMessages);
+        supportsMeasurements = view.findViewById(R.id.supportsMeasurements);
+        hasADR = view.findViewById(R.id.hasADR);
+        hasMsa = view.findViewById(R.id.hasMsa);
+        supportsSatellitePvt = view.findViewById(R.id.supportsSatellitePvt);
+        hardwareName = view.findViewById(R.id.gnssHardwareName);
+        hardwareYear = view.findViewById(R.id.gnssHardwareYear);
+        deviceLatitude = view.findViewById(R.id.deviceLatitude);
+        deviceAccuracy = view.findViewById(R.id.deviceAccuracy);
+        deviceBearing = view.findViewById(R.id.deviceBearing);
+        deviceLongitude = view.findViewById(R.id.deviceLongitude);
+        deviceAltitude = view.findViewById(R.id.deviceAltitude);
+        deviceBearingAccuracy = view.findViewById(R.id.deviceBearingAccuracy);
+
+        Activity act = getActivity();
+        if (act != null) act.runOnUiThread(this::Update);
     }
 
-    private void DrawCapabilities(View view) {
-        Activity activity = getActivity();
-        if (activity == null) {
-            Log.i("GeneralStatsPage", "The page is not selected, skipping");
-            return;
-        }
+    private void Update() {
+        // Set timers again
+        if (mRefreshTimer != null) mRefreshTimer.cancel();
+
+        mRefreshTimer = new Timer("GeneralStatsTimer");
+        mRefreshTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Activity act = getActivity();
+                if (act != null) {
+                    act.runOnUiThread(() -> DrawCapabilities());
+                }
+            }
+        }, 0, (6 * 333L));
+    }
+
+    private void DrawCapabilities() {
         if (Capabilities == null) {
             return;
         }
 
         // Navigation messages
-        TextView supportsNavigationMessages = view.findViewById(R.id.supportsNavigationMessages);
-        TextView supportsMeasurements = view.findViewById(R.id.supportsMeasurements);
-        TextView hasADR = view.findViewById(R.id.hasADR);
-        TextView hasMsa = view.findViewById(R.id.hasMsa);
-        TextView supportsSatellitePvt = view.findViewById(R.id.supportsSatellitePvt);
-        TextView hardwareName = view.findViewById(R.id.gnssHardwareName);
-        TextView hardwareYear = view.findViewById(R.id.gnssHardwareYear);
-        TextView deviceLatitude = view.findViewById(R.id.deviceLatitude);
-        TextView deviceLongitude = view.findViewById(R.id.deviceLongitude);
-        TextView deviceAltitude = view.findViewById(R.id.deviceAltitude);
-        TextView deviceAccuracy = view.findViewById(R.id.deviceAccuracy);
-        TextView deviceBearing = view.findViewById(R.id.deviceBearing);
-        TextView deviceBearingAccuracy = view.findViewById(R.id.deviceBearingAccuracy);
 
         // API 34 Data
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
-            assert hasADR != null;
             int support = Capabilities.hasAccumulatedDeltaRange();
             String adrText = "Unknown";
             switch (support) {
@@ -93,53 +115,69 @@ public class GeneralStatsPage extends Fragment {
                 case GnssCapabilities.CAPABILITY_SUPPORTED -> adrText = "Supported";
                 case GnssCapabilities.CAPABILITY_UNSUPPORTED -> adrText = "Unsupported";
             }
-            hasADR.setText(adrText);
 
-            hasMsa.setText(Capabilities.hasMsa() ? "Supported" : "Unsupported");
-            supportsSatellitePvt.setText(Capabilities.hasSatellitePvt() ? "Supported" : "Unsupported");
-        }
-        else {
-            hasADR.setText("Unknown");
-            hasMsa.setText("Unknown");
-            supportsSatellitePvt.setText("Unknown");
+            if (hasADR != null) {
+                hasADR.setText(adrText);
+            }
+            if (hasMsa != null) {
+                hasMsa.setText(Capabilities.hasMsa() ? "Supported" : "Unsupported");
+            }
+            if (supportsSatellitePvt != null) {
+                supportsSatellitePvt.setText(Capabilities.hasSatellitePvt() ? "Supported" : "Unsupported");
+            }
+        } else {
+            if (hasADR != null) {
+                hasADR.setText("Unknown");
+            }
+            if (hasMsa != null) {
+                hasMsa.setText("Unknown");
+            }
+            if (supportsSatellitePvt != null) {
+                supportsSatellitePvt.setText("Unknown");
+            }
         }
 
         // API 31 data
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            assert supportsNavigationMessages != null;
-            supportsNavigationMessages.setText(Capabilities.hasNavigationMessages() ? "Supported" : "Unsupported");
-
-            assert supportsMeasurements != null;
-            supportsMeasurements.setText(Capabilities.hasMeasurements() ? "Supported" : "Unsupported");
-        }
-        else {
-            supportsNavigationMessages.setText("Unknown");
-            supportsMeasurements.setText("Unknown");
+            if (supportsNavigationMessages != null) {
+                supportsNavigationMessages.setText(Capabilities.hasNavigationMessages() ? "Supported" : "Unsupported");
+            }
+            if (supportsMeasurements != null) {
+                supportsMeasurements.setText(Capabilities.hasMeasurements() ? "Supported" : "Unsupported");
+            }
+        } else {
+            if (supportsNavigationMessages != null) {
+                supportsNavigationMessages.setText("Unknown");
+            }
+            if (supportsMeasurements != null) {
+                supportsMeasurements.setText("Unknown");
+            }
         }
 
         // < API 30 data (supported by minimum)
-        assert hardwareYear != null;
-        hardwareYear.setText(GnssModelYear != 0 ? String.format(Locale.US , "%d", GnssModelYear) : "Unknown (before 2016)");
-
-        assert hardwareName != null;
-        hardwareName.setText(GnssModelName == null ? "Unknown" : GnssModelName);
-
-        assert deviceLatitude != null;
-        deviceLatitude.setText(String.valueOf(Latitude));
-
-        assert deviceLongitude != null;
-        deviceLongitude.setText(String.valueOf(Longitude));
-
-        assert deviceAltitude != null;
-        deviceAltitude.setText(String.valueOf(Altitude));
-
-        assert deviceAccuracy != null;
-        deviceAccuracy.setText(String.valueOf(Accuracy));
-
-        assert deviceBearing != null;
-        deviceBearing.setText(String.valueOf(Bearing));
-
-        assert deviceBearingAccuracy != null;
-        deviceBearingAccuracy.setText(String.valueOf(BearingAccuracy));
+        if (hardwareYear != null) {
+            hardwareYear.setText(GnssModelYear != 0 ? String.format(Locale.US, "%d", GnssModelYear) : "Unknown (before 2016)");
+        }
+        if( hardwareName != null) {
+            hardwareName.setText(GnssModelName == null ? "Unknown" : GnssModelName);
+        }
+        if (deviceLatitude != null) {
+            deviceLatitude.setText(String.valueOf(Latitude));
+        }
+        if (deviceLongitude != null) {
+            deviceLongitude.setText(String.valueOf(Longitude));
+        }
+        if (deviceAltitude != null) {
+            deviceAltitude.setText(String.valueOf(Altitude));
+        }
+        if (deviceAccuracy != null) {
+            deviceAccuracy.setText(String.valueOf(Accuracy));
+        }
+        if (deviceBearing != null) {
+            deviceBearing.setText(String.valueOf(Bearing));
+        }
+        if (deviceBearingAccuracy != null) {
+            deviceBearingAccuracy.setText(String.valueOf(BearingAccuracy));
+        }
     }
 }
