834cae96b115d00bc25f4cfec7d2783af85df720
Martí Climent <marticlilop@gmail.com>
2024-09-16 20:40:20 +0200

    bump version

--- GalileoFront/app/build.gradle.kts
+++ GalileoFront/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.gnsstracker.mainapp"
         minSdk = 29
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.0-b1"
+        versionCode = 4
+        versionName = "1.0.0-b2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
