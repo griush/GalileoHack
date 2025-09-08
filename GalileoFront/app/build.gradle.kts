857652a1f4f02da83a0363b9ac0417565b85101d
Martí Climent <marticlilop@gmail.com>
2025-09-08 16:50:33 +0200

    further improvements to translations

--- GalileoFront/app/build.gradle.kts
+++ GalileoFront/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.gnsstracker.mainapp"
         minSdk = 29
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.0.0-b10"
+        versionCode = 11
+        versionName = "1.0.0-b11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
